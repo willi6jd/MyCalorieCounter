56a37583a121bf57878218bfe64edb528f142de6
Kemal Ozturk <97kemalozturk@gmail.com>
2020-03-06 18:32:13 -0500

    Tried to get the code to build
    -The code didn't build simply because the UI element id's were not matching with the back end code.

--- app/src/main/java/edu/uc/willi6jd/mycaloriecounter/FoodActivity.kt
+++ app/src/main/java/edu/uc/willi6jd/mycaloriecounter/FoodActivity.kt
@@ -1,7 +1,6 @@
 package edu.uc.willi6jd.mycaloriecounter
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -21,29 +20,29 @@ class FoodActivity : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.food_listview)
         listView.adapter = foodCustomerAdapter(this)
 
-        val btnCreate = findViewById(R.id.btnCreateFoodItem) as Button //Create food item button
+        val btnCreate = findViewById<Button>(R.id.btnCreateFoodItem) //Create food item button
 
         btnCreate.setOnClickListener {
 
             // Get input fields. Store them to food item variables
-            var itemName = findViewById<EditText>(R.id.foodName); //Name of food item: input text field
-            var itemCalories = findViewById<EditText>(R.id.foodCalories); //Calorie amount of food item: integer
-            var itemSugar = findViewById<EditText>(R.id.foodSugar); //Sugar amount of food item: integer
-            var itemProtein = findViewById<EditText>(R.id.foodProtein); //Protein amount of food: integer
+            var itemName = findViewById<EditText>(R.id.listFoodName) //Name of food item: input text field
+            var itemCalories = findViewById<EditText>(R.id.listFoodCal) //Calorie amount of food item: integer
+            var itemSugar = findViewById<EditText>(R.id.listFoodSugar) //Sugar amount of food item: integer
+            var itemProtein = findViewById<EditText>(R.id.listFoodPro) //Protein amount of food: integer
 
             // Put food item variables into foodItem Object
             fun foodItem(nm: String, cal: Int, sgr: Int, pro: Int) {
-                FoodItem.name = nm;
-                FoodItem.calories = cal;
-                FoodItem.sugar = sgr;
-                FoodItem.protein = pro;
+                FoodItem.name = nm
+                FoodItem.calories = cal
+                FoodItem.sugar = sgr
+                FoodItem.protein = pro
             }
 
             // Build FoodItem Array
             val foodItemArray = Array(20) { }
 
             // Push foodItem to FoodItem Array
-            foodItemArray.add(foodItem(nm, cal, sgr, pro))
+            //foodItemArray.add(foodItem(nm, cal, sgr, pro))
         }
     }
 
@@ -91,7 +90,7 @@ private class foodCustomerAdapter(context: Context): BaseAdapter(){
             val calTextView = rowMain.findViewById<TextView>(R.id.listFoodCal)
             calTextView.text = cal.get(position)
 
-            val fatTextView = rowMain.findViewById<TextView>(R.id.listFoodFat)
+            val fatTextView = rowMain.findViewById<TextView>(R.id.listFoodSugar)
             fatTextView.text = fat.get(position)
 
             val proTextView = rowMain.findViewById<TextView>(R.id.listFoodPro)
@@ -102,10 +101,10 @@ private class foodCustomerAdapter(context: Context): BaseAdapter(){
 
     }
     object FoodItem {
-        var name = "";
-        var calories = 0;
-        var sugar = 0;
-        var protein = 0;
-    }
+        var name = ""
+        var calories = 0
+        var sugar = 0
+        var protein = 0
+    }}
 
 
